7591ac1f8c920541c69c7baeb08a11c00e15a3ac
Lin Kayser <github@lsk.me>
2023-10-21 21:03:33 +0900

    Fixed issues in Windows dependency installer
    Installing Bootstrap now a subroutine call and changed order of dependency installs

--- Install_Dependencies/Win.bat
+++ Install_Dependencies/Win.bat
@@ -1,13 +1,13 @@
 if not exist vcpkg\ (
 	git clone https://github.com/microsoft/vcpkg
-	.\vcpkg\bootstrap-vcpkg.bat
+	call .\vcpkg\bootstrap-vcpkg.bat
 )
 
-.\vcpkg\vcpkg install zlib:x64-windows
-.\vcpkg\vcpkg install blosc:x64-windows
-.\vcpkg\vcpkg install tbb:x64-windows
 .\vcpkg\vcpkg install boost-iostreams:x64-windows
 .\vcpkg\vcpkg install boost-any:x64-windows
 .\vcpkg\vcpkg install boost-algorithm:x64-windows
 .\vcpkg\vcpkg install boost-uuid:x64-windows
 .\vcpkg\vcpkg install boost-interprocess:x64-windows
+.\vcpkg\vcpkg install zlib:x64-windows
+.\vcpkg\vcpkg install blosc:x64-windows
+.\vcpkg\vcpkg install tbb:x64-windows
