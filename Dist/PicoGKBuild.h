07dfdd449f39dc64e0a56c50dd48b1f70171b1cd
Lin Kayser <github@lsk.me>
2023-11-18 21:28:17 +0400

    Removed distribution directory
    We will have official installers soon

--- Dist/PicoGKBuild.h
+++
@@ -1,8 +0,0 @@
-#ifndef PICOGK_BUILD_H
-#define PICOGK_BUILD_H
-
-#define PICOGK_BUILD       "2023-11-15 22:21:39"
-#define PICOGK_LIB_VERSION "1.0"
-#define PICOGK_LIB_NAME    "picogk.1.0"
-
-#endif // PICOGK_BUILD_H
