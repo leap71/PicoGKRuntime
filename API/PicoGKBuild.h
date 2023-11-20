60cc88ca7f529186c973f6308590cbdfc5fbe6fd
Lin Kayser <github@lsk.me>
2023-11-20 15:09:09 +0400

    Added version name to lib on Win
    *nix type systems add it automatically, but we need to append it on Win

--- API/PicoGKBuild.h
+++ API/PicoGKBuild.h
@@ -1,8 +1,8 @@
 #ifndef PICOGK_BUILD_H
 #define PICOGK_BUILD_H
 
-#define PICOGK_BUILD       "2023-11-19 18:30:41"
+#define PICOGK_BUILD       "2023-11-20 15:03:36"
 #define PICOGK_LIB_VERSION "1.1.0"
-#define PICOGK_LIB_NAME    "picogk"
+#define PICOGK_LIB_NAME    "picogk.1.1"
 
 #endif // PICOGK_BUILD_H
