b532f4b4fe1c9f9919ebaabd5d0094868c072e44
Lin Kayser <github@lsk.me>
2024-02-12 13:08:02 +0400

    Version number bumped to 1.5

--- API/PicoGKBuild.h
+++ API/PicoGKBuild.h
@@ -1,8 +1,8 @@
 #ifndef PICOGK_BUILD_H
 #define PICOGK_BUILD_H
 
-#define PICOGK_BUILD       "2024-02-08 16:43:16"
-#define PICOGK_LIB_VERSION "1.3.0"
+#define PICOGK_BUILD       "2024-02-12 13:06:00"
+#define PICOGK_LIB_VERSION "1.5.0"
 #define PICOGK_LIB_NAME    "picogk"
 
 #endif // PICOGK_BUILD_H
