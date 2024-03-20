526605a7c5fb9d5a6d23639e2e70249bcbdef465
Lin Kayser <github@lsk.me>
2024-03-20 14:14:44 +0400

    Updated PicoGK copyright notice

--- API/PicoGKBuild.h
+++ API/PicoGKBuild.h
@@ -1,7 +1,7 @@
 #ifndef PICOGK_BUILD_H
 #define PICOGK_BUILD_H
 
-#define PICOGK_BUILD       "2024-03-20 09:52:38"
+#define PICOGK_BUILD       "2024-03-20 14:13:02"
 #define PICOGK_LIB_VERSION "1.5.0"
 #define PICOGK_LIB_NAME    "picogk"
 
