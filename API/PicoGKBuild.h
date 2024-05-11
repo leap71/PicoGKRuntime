745e8d8c4e8e896455312c468c2f3b3a2bd37ca6
Lin Kayser <github@lsk.me>
2024-05-11 17:15:50 +0400

    Update PicoGKBuild.h
    Office 1.5 build

--- API/PicoGKBuild.h
+++ API/PicoGKBuild.h
@@ -1,7 +1,7 @@
 #ifndef PICOGK_BUILD_H
 #define PICOGK_BUILD_H
 
-#define PICOGK_BUILD       "2024-05-10 19:37:14"
+#define PICOGK_BUILD       "2024-05-11 17:14:04"
 #define PICOGK_LIB_VERSION "1.5.0"
 #define PICOGK_LIB_NAME    "picogk"
 
