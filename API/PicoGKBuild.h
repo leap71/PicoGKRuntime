09e5f01fb81bae7b015ba931a3638f777a9cbc9f
Lin Kayser <148448288+LinKayser@users.noreply.github.com>
2025-07-19 18:21:40 +0400

    New versioning scheme - YY.VV.PP

--- API/PicoGKBuild.h
+++ API/PicoGKBuild.h
@@ -1,8 +1,8 @@
 #ifndef PICOGK_BUILD_H
 #define PICOGK_BUILD_H
 
-#define PICOGK_BUILD       "2024-08-03 18:24:24"
-#define PICOGK_LIB_VERSION "1.7.1"
+#define PICOGK_BUILD       "2025-07-19 18:05:54"
+#define PICOGK_LIB_VERSION "25.2.0"
 #define PICOGK_LIB_NAME    "picogk"
 
 #endif // PICOGK_BUILD_H
