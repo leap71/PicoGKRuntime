74519cd042c3f860d454950411b5f775abdaba34
Lin Kayser <148448288+LinKayser@users.noreply.github.com>
2025-08-05 19:06:01 +0100

    Update PicoGKBuild.h

--- API/PicoGKBuild.h
+++ API/PicoGKBuild.h
@@ -1,7 +1,7 @@
 #ifndef PICOGK_BUILD_H
 #define PICOGK_BUILD_H
 
-#define PICOGK_BUILD       "2025-08-04 16:28:05"
+#define PICOGK_BUILD       "2025-08-04 22:20:20"
 #define PICOGK_LIB_VERSION "25.2.0"
 #define PICOGK_LIB_NAME    "picogk"
 
