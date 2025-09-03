22e2296ddc643d5fd9d6544b5d418a164d20390b
Lin Kayser <148448288+LinKayser@users.noreply.github.com>
2025-09-03 19:36:25 +0800

    Updated openvdb

--- API/PicoGKBuild.h
+++ API/PicoGKBuild.h
@@ -1,7 +1,7 @@
 #ifndef PICOGK_BUILD_H
 #define PICOGK_BUILD_H
 
-#define PICOGK_BUILD       "2025-08-31 22:17:45"
+#define PICOGK_BUILD       "2025-09-03 19:25:24"
 #define PICOGK_LIB_VERSION "25.2.0"
 #define PICOGK_LIB_NAME    "picogk"
 
