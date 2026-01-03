ebef9345c04f8fe3ffe4406da32707b291841748
Lin Kayser <148448288+LinKayser@users.noreply.github.com>
2026-01-03 19:11:27 +0400

    Updated to 2026
    Updated submodules, copyright, version

--- API/PicoGKBuild.h
+++ API/PicoGKBuild.h
@@ -1,8 +1,8 @@
 #ifndef PICOGK_BUILD_H
 #define PICOGK_BUILD_H
 
-#define PICOGK_BUILD       "2025-10-08 16:28:35"
-#define PICOGK_LIB_VERSION "25.2.0"
+#define PICOGK_BUILD       "2026-01-03 19:09:48"
+#define PICOGK_LIB_VERSION "26.1.0"
 #define PICOGK_LIB_NAME    "picogk"
 
 #endif // PICOGK_BUILD_H
