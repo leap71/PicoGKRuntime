0541ffc752b04c51dab7acb14f5d0e8cc1b243db
Lin Kayser <148448288+LinKayser@users.noreply.github.com>
2026-02-20 14:32:55 +0400

    Prepare for release

--- API/PicoGKBuild.h
+++ API/PicoGKBuild.h
@@ -1,8 +1,8 @@
 #ifndef PICOGK_BUILD_H
 #define PICOGK_BUILD_H
 
-#define PICOGK_BUILD       "2026-01-30 17:30:34"
-#define PICOGK_LIB_VERSION "26.1.0"
+#define PICOGK_BUILD       "2026-02-20 13:38:52"
+#define PICOGK_LIB_VERSION "26.1.1"
 #define PICOGK_LIB_NAME    "picogk"
 
 #endif // PICOGK_BUILD_H
